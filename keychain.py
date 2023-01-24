d1a2557a86111eec5dd4e60fe4eeb19c58bc0a68
Derek Coleman <derek@derekcoleman.com>
2023-01-23 22:55:33 -0500

    updated main.py

--- keychain.py
+++ keychain.py
@@ -1,11 +1,7 @@
-from keychain import Keychain
+from Foundation import NSData
+from PyObjCTools import AppHelper
+from objc import *
 
-# Create a new instance of the Keychain class
-keychain = Keychain()
-
-# Add a password to the keychain
-keychain.add_password("example_account", "example_password")
-
-# Retrieve a password from the keychain
-password = keychain.get_password("example_account")
-print(password)
+class Keychain:
+    def __init__(self):
+        self.keychain = ObjCClass('Keychain
